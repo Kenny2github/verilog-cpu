3f1d7234fc3cc5d5eee3d1b0ea2c9cda23d074cc
AbyxDev <ken@abyx.dev>
2022-12-15 17:37:00 -0500

    Let assembler take filename arguments

--- assembler/__main__.py
+++ assembler/__main__.py
@@ -6,24 +6,38 @@ from .assemble import mkbytes
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(dest='cmd', required=True)
 hex_parser = subparsers.add_parser('to_hex')
+hex_parser.add_argument('infile', default='-')
+hex_parser.add_argument('outfile', default='-')
 
-def to_hex():
-    text = sys.stdin.read()
+def to_hex(infile: str, outfile: str) -> None:
+    if infile == '-':
+        text = sys.stdin.read()
+    else:
+        with open(infile, 'r') as f:
+            text = f.read()
+    if outfile == '-':
+        out = sys.stdout
+    else:
+        out = open(outfile, 'w')
     data = mkbytes(text)
     wordsperline = 25
     print(f"""
 // memory data file (do not edit the following line - required for mem load use)
 // instance=/cpu/u_datapath/u_MEM0/altsyncram_component/m_default/altsyncram_inst/mem_data
 // format=hex addressradix=h dataradix=h version=1.0 wordsperline={wordsperline}
-""".strip())
+""".strip(), file=out)
     remainder = len(data) % wordsperline
     rows = chain(zip(*[iter(data)]*wordsperline), [data[-remainder:]])
     for addr, row in enumerate(rows):
+        if not row:
+            continue
         addr = f'@{addr * wordsperline:x}'.rjust(3)
         row = ' '.join(f'{byte:02x}' for byte in row)
-        print(f'{addr} {row}')
+        print(f'{addr} {row}', file=out)
+    if outfile != '-':
+        out.close()
 
 args = parser.parse_args()
 
 if args.cmd == 'to_hex':
-    to_hex()
+    to_hex(args.infile, args.outfile)
